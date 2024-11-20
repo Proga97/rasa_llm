180bac0fd41d91c9f9f21123b4201bf61b1e33bd
alexander-brady <bradya@student.ethz.ch>
2024-11-20 12:30:20 -0500

    alexander refurbish

--- actions/actions.py
+++ actions/actions.py
@@ -1,95 +1,33 @@
-import random
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
-from open_ai_detection import detect_pun_word,detect_pun_word_match
-from word_match import find_similar_sounding_words_and_phrases
+from rasa_sdk.events import AllSlotsReset
+
+from pun_detection import chatbot_response
 
 
-class ActionCheckPunSentence(Action):
+class ActionExplainPun(Action):
     def name(self) -> Text:
-        return "action_check_pun_sentence"
-
+        return "action_explain_pun"
+    
     def run(
         self,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-
-        pun_sentence = tracker.get_slot("pun_sentence")
-        if pun_sentence is None:
-            dispatcher.utter_message(
-                text="It seems you haven't provided a pun yet. Can you please try again?"
-            )
-            return []
-        print("Pun sentence = " + "   "  + pun_sentence or "pun slot empty")
-        is_pun_detected,pun_word  = self.detect_pun(pun_sentence)
-        print("is_pun_detected = " + "   "  + str(is_pun_detected or "true/false") + "   " + str(pun_word or "no pun word foundzz" + "   " ))
-        # if is_pun_detected:
-        #     dispatcher.utter_message(text="Pun detected! Parsing the pun...")
-        # else:
-        #     dispatcher.utter_message(text="No pun detected. Please try another sentence.")
-
-        return [
-        SlotSet("pun_detected", is_pun_detected),
-        SlotSet("pun_word", pun_word)]
-
-
-    def detect_pun(self, sentence: str):
-
-        ############# Alex's pun word detection algo goes here;
-
-        # x = random.choice([True, False]) or True
-        # return  x  , "pun_word" if x else None
-        print(sentence)
-        text = detect_pun_word(sentence)
-        x = text.split(",")
-        print("gpt ans = "  + x[0] == 'True' + x[1])
-
-        #################### Alex's pun word detection algo goes here;
-
-        return x[0],x[1]
-    
-class ActionCheckMatchingPunWord(Action):
-    def name(self) -> Text:
-        return "action_detect_matching_pun_word"
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
-
-        pun_word = tracker.get_slot("pun_word")
-        print("Pun matcherr " + str(pun_word or "missing word"))
-
-        is_pun__match_detected ,matched_word  = self.detect_similar_word(pun_word)
+        joke = tracker.get_slot("joke")
         
-        # if is_pun__match_detected:
-        #     dispatcher.utter_message(text="Pun matched!")
-        #     dispatcher.utter_message(text="The matched word is " + matched_word)
-        # else:
-        #     dispatcher.utter_message(text="Oops couldn't find the matching pun word. Please try another sentence.")
-        return [SlotSet("pun__match_detected", (is_pun__match_detected or False)),
-        SlotSet("matched_pun_word", matched_word)]
-
+        response = chatbot_response(joke)
         
-
-    def detect_similar_word(self, sentence: str):
-        ############# Hassan and Denison's pun word detection algo goes here;
-        # x = random.choice([True, False])
-        # return [ x , "matched_pun_word" if x else None]
-
-        # text = detect_pun_word_match(sentence)
-        text = find_similar_sounding_words_and_phrases(sentence)
-        # x = text.split(",")
-        print(text)
-        # print("gpt ans = "  + x[0] == 'True' + x[1])
-
-        ############# Hassan and Denison's pun word detection algo goes here;
-
-        # return x[0],x[1]
-    
+        if response:
+            dispatcher.utter_message(response)
+            
+        else:
+            dispatcher.utter_message(response='utter_explain_joke')
+        
+        return [AllSlotsReset()] 
+        
+        
+        
+        
---
+++ pun_detection/detect_puns.py
@@ -0,0 +1,138 @@
+from guidance import models, gen, assistant, user, system, select, substring
+from pun_detection import get_homophones
+
+from nltk.corpus import wordnet as wn
+
+# Chatbot response
+system_should_explain_joke = 'Is the user asking you to explain a joke or pun?'
+system_extract_pun = '''Which words in the user message make up the pun? 
+Example: 
+Q: Please explain this joke: "I used to be a baker, but I couldn\'t make enough dough."
+A: The pun word(s) is: dough'''
+system_pun_type = '''Is the  humor in the pun based on "sound" or "meaning"?
+Example:
+The pun "What do you call a fake noodle? An impasta." is based on "sound", because "impasta" sounds like "imposter".
+The pun "My wife told me to stop impersonating a flamingo. I had to put my foot down." is based on "meaning", because "put my foot down" is an idiom meaning to assert authority.'''
+
+# Explain joke
+system_explain_joke = 'Explain the humor in the joke or pun.'
+
+error_message = None
+
+path = 'Llama-3.2-3B-Instruct-Q6_K_L.gguf'
+
+llama = models.LlamaCpp(path, n_ctx=4096)
+
+def explain_joke(lm):
+    with system():
+        llm = lm + system_explain_joke
+    with assistant():
+        llm = gen('explain_joke')
+        if 'explain_joke' in llm:
+            return llm['explain_joke']
+        else:
+            return error_message
+    
+
+def handle_homograph(lm, pun):
+    
+    synsets = wn.synsets(pun)
+    definitions = [syn.definition() for syn in synsets]
+    
+    if len(definitions) < 2:
+        return explain_joke(lm)
+        
+    elif len(definitions) == 2:
+        def1, def2 = definitions
+    
+    else:
+        with system():
+            llm = lm + 'The pun word "' + pun + '" plays on multiple meanings. Of the following dictionary definitions, which two explain the pun? \n - ' + '\n - '.join(definitions)
+            
+        with assistant():
+            llm += 'The humor plays off of the pun "' + pun + '" having two distinct meanings in this context. First, the definition "' + select(definitions, name = 'def1') + '"'
+            if 'def1' in llm:
+                def1 = llm['def1']
+                definitions.remove(def1)
+                llm += '. Second, "' + pun + '" also has the definition: "' + select(definitions, name = 'def2')
+                if 'def2' in llm:
+                    def2 = llm['def2']
+                else:
+                    return explain_joke(lm)
+            else:
+                return explain_joke(lm)
+        
+    with assistant():
+        lm += 'The pun "' + pun + '" is a homograph. It plays off the two meanings of the word "' + pun + '": (1) ' + def1 + ' and (2) ' + def2 + '. ' + gen(stop='.', name='explanation')
+        return 'The pun "' + pun + '" is a homograph. It plays off the two meanings of the word "' + pun + '": (1) ' + def1 + ' and (2) ' + def2 + '. ' + (lm['explanation'] + '.' if 'explanation' in lm else '')
+    
+def handle_homophone(lm, pun):
+    
+    cleansed_pun = pun.replace('-', ' ').lower()
+    cleansed_pun = ''.join([c for c in cleansed_pun if c.isalpha() or c.isspace()])
+    
+    similar_phones = get_homophones(cleansed_pun)
+    
+    if len(similar_phones) == 0:
+        return explain_joke(lm)
+    
+    elif len(similar_phones) == 1:
+        with assistant():
+            homophone = similar_phones[0]
+                                
+    else:
+        with system():
+            llm = lm + 'The pun word "' + pun + '" sounds like multiple word(s). Which one is the pun? \n - ' + '\n - '.join(similar_phones)
+        
+        with assistant():
+            llm += 'The pun "' + pun + '" is a pun because it sounds like the following word(s): ' + select(similar_phones, name = 'homophone')
+        
+        if 'homophone' not in llm:
+            return explain_joke(lm)
+                
+        homophone = llm['homophone']
+        
+    with assistant():
+        lm += 'The joke relies on the homophone "' + pun + '", which sounds like "' + homophone + '". ' + gen(stop='.', name='explanation')
+        return 'The joke relies on the homophone "' + pun + '", which sounds like "' + homophone + '". ' + (lm['explanation'] + '.' if 'explanation' in lm else '')
+
+def explain_joke(joke):
+    
+    with system():
+        lm = llama + system_chatbot    
+    
+    with user():
+        lm += 'Please explain this joke: "' + joke + '"'
+                
+    with system():
+        lm += system_extract_pun
+        
+    with assistant():
+        lm += 'The pun word(s) is: ' + substring(joke, name='pun')
+    
+    if 'pun' in lm:
+        pun = lm['pun'].strip()
+        
+        with system():
+            lm += system_pun_type
+        
+        with assistant():
+            lm += select(['sound', 'meaning'], name='pun_type')
+        
+        if 'pun_type' in lm:
+            
+            if lm['pun_type'] == 'sound':
+                return handle_homophone(lm, pun)
+                                    
+            else:
+                return handle_homograph(lm, pun)
+    
+    with assistant():
+        lm += gen('explanation')
+        
+    if 'explanation' in lm:
+        return lm['explanation']
+
+    else:
+        return error_message
+    
